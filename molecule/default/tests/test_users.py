2793a7f334b573b9a24fbf52129b6ea8a0eaa095
Mike Pettypiece <mike@pettypiece.com>
2021-03-17 13:24:55 -0400

    Add authorized_keys

--- molecule/default/tests/test_users.py
+++ molecule/default/tests/test_users.py
@@ -8,6 +8,8 @@ def test_users(host):
     assert test1.exists
     assert test1.shell == "/bin/zsh"
 
+    assert host.file("/home/test1/.ssh/authorized_keys").exists
+
     test2 = host.user("test2")
     assert test2.exists
     assert test2.shell == "/bin/bash"
